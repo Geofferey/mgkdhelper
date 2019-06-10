56b239d1b4224ab0ade96c7243f494abc7d501cc
geofferey <geofferey.eakins@gmail.com>
2019-06-10 10:58:04 +0000

    Contribs from jcmm11@xda-devs

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -6,9 +6,4 @@
 # if Magisk change its mount point in the future
 MODDIR=${0%/*}
 
-# This script will be executed in late_start service mode
-#Magisk Daemon Helper
-# Copyright (C) 2019 Geofferey @ XDA
-# License: GPL V3+
-
-nohup $MODDIR/script/mgkd-helper > /dev/null &
+# This script will be executed in post-fs-data mode
